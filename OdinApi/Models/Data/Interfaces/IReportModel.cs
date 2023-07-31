690e15804cb078651a127e25d3c8e1b78b1913c6
jsand <jsandicr16@gmail.com>
2023-07-30 21:38:27 -0600

    Reportes y graficos

---
+++ OdinApi/Models/Data/Interfaces/IReportModel.cs
@@ -0,0 +1,12 @@
+﻿using OdinApi.Models.Obj;
+
+namespace OdinApi.Models.Data.Interfaces
+{
+    public interface IReportModel
+    {
+        public List<Ticket> GetTicketsXTime();
+        public List<Ticket> GetTicketsXSupervisor();
+        public int GetCantTicketsAssigned(int id);
+        public int GetCantTicketsOpen();
+    }
+}
