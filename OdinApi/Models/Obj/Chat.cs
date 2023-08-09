b41a3c841fccba692c75b345d8bf579f98ef5f76
Xerarth19 <101201557+Xerarth19@users.noreply.github.com>
2023-08-08 23:03:57 -0600

    chat

---
+++ OdinApi/Models/Obj/Chat.cs
@@ -0,0 +1,10 @@
+﻿namespace OdinApi.Models.Obj
+{
+    public class Chat
+    {
+        public int Id { get; set; }
+        public string Text { get; set; } = null;
+        public string Answer { get; set; } = null;
+    }
+}
+
